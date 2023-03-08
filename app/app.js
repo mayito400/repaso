67d6339aeaf13934dd9058a65a9b2ae7137a4b5e
f69 <f69>
2023-03-08 17:24:26 -0500

    Recorderis

---
+++ app/app.js
@@ -0,0 +1,13 @@
+//import { express } 
+ //const express=require("express");
+ import express from "express";
+ import message from "./config/message";
+ const app = express();
+ 
+app.listen(8080, ()=>{
+    //console.log("hOLAaaaaa")
+    message("Hola Mundo", "danger")
+})
+/*
+Babel al instalarse viene con las actualizaciones reciente de EMJ
+ */
