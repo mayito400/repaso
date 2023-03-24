85f989d1371b030355d866c7d526b7b69b863e08
Mafe <mafess464@gmail.com>
2023-03-24 17:11:29 -0500

    24

--- app/app.js
+++ app/app.js
@@ -3,9 +3,12 @@
  import express from "express";
  import message from "./config/message";
  import enviroment from "./config/enviroment";
+ import allRoutes from "./routes/user.routes";
  const app = express();
  app.set("PORT",process.env.PORT || 3000) ;
  
+ app.use(express.json());
+ app.use("/api",allRoutes);
 /*
 Babel al instalarse viene con las actualizaciones reciente de EMJ
  */
---
+++ app/routes/user.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import * as controller from "../controllers/user.controller"
+
+const route = Router();
+
+route.get("/", (req, res)=>{
+    res.send("<h1>Hola mundo</h1>");
+});
+
+route.get("/user", controller.findAllUser);
+route.get("/user/:id", controller.findUser);
+route.post("/user/", controller.insertUser);
+route.put("/user", controller.updateUser);
+route.delete("/user/:id", controller.deleteUser);
+
+
+  
+export default route;
